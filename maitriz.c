af0197aa9d1cc3a744a7a1073018ae8f04846186
Laboratorio <laboratorio@udla-ec.int>
2023-11-22 16:23:48 -0500

    pide al usuario el numero de filas y columnas de la matriz

---
+++ maitriz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int filas, columnas;
+    int matriz;
+    int i,j;
+    printf("Ingrese el numero de filas: ");
+    scanf("%d", &filas);
+    
+    printf("Ingrese el numero de columnas: ");
+    scanf("%d", &columnas);
+    
+    return 0;
+}
