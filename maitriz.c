2a66fd5b79041666065361e37f9821b90ddfdf4d
ronny quispe <ronny.quispe@udla.eu.ec>
2023-11-22 16:40:27 -0500

    arreglo de estructura de codigo

--- maitriz.c
+++ maitriz.c
@@ -1,36 +1,42 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int fila, columna;
-    int i,j;
+    int i, j;
     printf("Ingrese el numero de filas: ");
     scanf("%d", &fila);
-    
+
     printf("Ingrese el numero de columnas: ");
     scanf("%d", &columna);
-    
+
     int matriz[fila][columna];
-    
+
     // Inicializar la matriz en ceros
-    for ( i = 0; i < fila; i++) {
-        for ( j = 0; j < columna; j++) {
+    for (i = 0; i < fila; i++)
+    {
+        for (j = 0; j < columna; j++)
+        {
             matriz[i][j] = 0;
         }
     }
-    
-        // Establecer la diagonal principal en 1
-    for ( i = 0; i < fila; i++) {
+
+    // Establecer la diagonal principal en 1
+    for (i = 0; i < fila; i++)
+    {
         matriz[i][i] = 1;
     }
-    
+
     // Imprimir la matriz
     printf("Matriz resultante de la matriz %d por %d  es:\n", fila, columna);
-    for ( i = 0; i < fila; i++) {
-        for ( j = 0; j < columna; j++) {
+    for (i = 0; i < fila; i++)
+    {
+        for (j = 0; j < columna; j++)
+        {
             printf("%d ", matriz[i][j]);
         }
         printf("\n");
     }
-    
+
     return 0;
 }
