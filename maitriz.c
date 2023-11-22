534ff83d4ef74ce8a02a31a56e761e30a95c19e3
Laboratorio <laboratorio@udla-ec.int>
2023-11-22 16:29:39 -0500

    inicio un buche FOR para que la diagonal principal sea 1

--- maitriz.c
+++ maitriz.c
@@ -2,7 +2,7 @@
 
 int main() {
     int fila, columna;
-    
+    int i,j;
     printf("Ingrese el numero de filas: ");
     scanf("%d", &fila);
     
@@ -12,10 +12,16 @@ int main() {
     int matriz[fila][columna];
     
     // Inicializar la matriz en ceros
-    for (int i = 0; i < fila; i++) {
-        for (int j = 0; j < columna; j++) {
+    for ( i = 0; i < fila; i++) {
+        for ( j = 0; j < columna; j++) {
             matriz[i][j] = 0;
         }
     }
+    
+        // Establecer la diagonal principal en 1
+    for ( i = 0; i < fila; i++) {
+        matriz[i][i] = 1;
+    }
+  
     return 0;
 }
